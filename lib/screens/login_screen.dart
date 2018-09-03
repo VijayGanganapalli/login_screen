9709e0172770b0b7fb7306d1b5f2a59c7da24047
Vijay <vijayganganapalli@gmail.com>
2018-09-03 19:28:49 +0530

    text form field added

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -9,9 +9,44 @@ class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Here a form!'),
+      body: Container(
+        margin: EdgeInsets.all(18.0),
+        child: Form(
+          child: Column(
+            children: <Widget>[
+              emailField(),
+              passwordField(),
+              submitButton(),
+            ],
+          ),
+        ),
       ),
+    );
+  }
+
+  Widget emailField() {
+    return TextFormField(
+      keyboardType: TextInputType.emailAddress,
+      decoration: InputDecoration(
+          labelText: 'Email Address', hintText: 'you@example.com'),
+    );
+  }
+
+  Widget passwordField() {
+    return TextFormField(
+      obscureText: false,
+      decoration: InputDecoration(
+        labelText: 'Password',
+        hintText: 'Password',
+      ),
+    );
+  }
+
+  Widget submitButton() {
+    return RaisedButton(
+      color: Colors.blue[200],
+      child: Text('Submit'),
+      onPressed: () {},
     );
   }
 }
